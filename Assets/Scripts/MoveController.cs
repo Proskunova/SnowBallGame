b78d3341589ed8fa0f8dec4a7d65ca7249c27c88
Proskunova <Proskunova.K@gmail.com>
2021-11-09 21:26:38 +0200

    Добавила кнопки. Добавила джойстик. Добавила MoveController  + Settings

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] Settings settings;
+    [SerializeField] SimpleInputNamespace.Joystick joystick;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * settings.Speed * joystick.Value);
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Character Settings")]
+public class Settings : ScriptableObject
+{
+    public float Speed;
+}
